e3903b2f60b2fbacde967bc742e63f4faa76619a
Arjun Singh <101566727+ArjunSinghBhakuni@users.noreply.github.com>
2022-06-15 20:39:09 +0530

    90%

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './component/Navbar';
-import HomeSliding from './component/HomeSliding';
-import Welcome from './component/Welcome';
-import Location from './component/Location';
 
 function App() {
   return (
-    <div >
-   <Navbar/>
-   <Location/>
-   <HomeSliding/>
-   <Welcome/>
+    <div  >
+    <Navbar/>
     </div>
   );
 }
