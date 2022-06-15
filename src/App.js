c44a0a228234c0c2d259ddfe73b2cb7cb34375e9
ArjunSinghBhakuni <arjunbhakuni23@gmail.com>
2022-06-16 00:33:32 +0530

    routes added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Location from './components/Location';
-import HomeSliding from './components/HomeSliding';
-import Welcome from './components/Welcome';
+import AllRoutes from './pages/AllRoutes';
+
+
 
 function App() {
   return (
     <div  >
-    <Navbar/>
-    <Location/>
-    <HomeSliding/>
-    <Welcome/>
-    <Footer/>
+   <AllRoutes/>
     </div>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import Location from '../components/Location';
+import HomeSliding from '../components/HomeSliding';
+import Welcome from '../components/Welcome';
+
+const Home = () => {
+  return (
+    <div>
+  
+    <Location/>
+    <HomeSliding/>
+    <Welcome/>
+ 
+
+    </div>
+  )
+}
+
+export default Home
