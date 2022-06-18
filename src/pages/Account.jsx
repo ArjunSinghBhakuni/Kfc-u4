26a8e89116065b598f44f845c14574b6ef7f0bfd
ArjunSinghBhakuni <arjunbhakuni23@gmail.com>
2022-06-18 19:51:02 +0530

    sidebar

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,15 +1,17 @@
 import { Button } from '@chakra-ui/react'
 import React from 'react'
 import { useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { AuthContext } from '../context/AuthConext'
 
 const Account = () => {
   const [isAuth, dispatch] = useContext(AuthContext);
-
+const navigate = useNavigate()
   const handleClick =()=>{
     dispatch({
-      type:"HANDLELOUT"
+      type:"HANDLELOGOUT"
     })
+    navigate("/")
     console.log(isAuth)
   }
   return (
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 import Location from '../components/Location';
 import HomeSliding from '../components/HomeSliding';
 import Welcome from '../components/Welcome';
-
+import {SimpleSidebar} from'./SimpleSidebar'
 const Home = () => {
   return (
     <div>
-  
+  <SimpleSidebar/>
     <Location/>
     <HomeSliding/>
     <Welcome/>
